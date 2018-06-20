52df24e8cfb4b5c67484396821b4587e52385d81
Matthias Hryniszak <padcom@gmail.com>
2018-06-20 04:23:43 +0200

    Code cleanup

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import Defocuser from 'defocuser-vue'
 
-console.log('main.js', Defocuser)
-
 Vue.config.productionTip = false
 Vue.use(Defocuser)
 
