a20e1994a1fa69fd4e616f4dd0c9e7303225fb9e
Matthias Hryniszak <padcom@gmail.com>
2018-06-20 20:09:38 +0200

    Added additional example of how it is working with prevent and stop modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <a href="#">exercitation</a>  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <div class="dropdown" name="me">
-      <span class="label" ref="label" @click="toggle">click me!</span>
-      <div v-if="dropdown" class="dropdown-content" v-defocus.capture="close" v-defocus-secondary="$refs.label">
+      <span class="label" ref="label1" @click="toggle('dropdown1')">click me!</span>
+      <div v-if="dropdown1" class="dropdown-content" v-defocus.prevent.capture="close1" v-defocus-secondary="$refs.label1">
         Hello, world!
       </div>
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <a href="#">exercitation</a>  ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="dropdown" name="me">
+      <span class="label" ref="label2" @click="toggle('dropdown2')">click me!</span>
+      <div v-if="dropdown2" class="dropdown-content" v-defocus.stop.capture="close2" v-defocus-secondary="$refs.label2">
+        Hello, world!
+      </div>
+    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <a href="#">exercitation</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </template>
 
@@ -16,15 +23,19 @@ export default {
   name: 'app',
   data () {
     return {
-      dropdown: false
+      dropdown1: false,
+      dropdown2: false
     }
   },
   methods: {
-    toggle () {
-      this.dropdown = !this.dropdown
+    toggle (dropdown) {
+      this[dropdown] = !this[dropdown]
     },
-    close () {
-      this.dropdown = false
+    close1 () {
+      this.dropdown1 = false
+    },
+    close2 () {
+      this.dropdown2 = false
     }
   }
 }
